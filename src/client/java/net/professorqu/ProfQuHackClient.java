0c37a490ac3ebcaab39c42eb19e8b41fdcf2d6fd
ProfessorQu <quint@dasookwat.com>
2024-03-12 14:07:03 +0100

    Renamed everything
    Renamed everything

--- src/client/java/net/professorqu/ProfQuHackClient.java
+++ src/client/java/net/professorqu/ProfQuHackClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package net.professorqu;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class ProfQuHackClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
